4b0fd17e81bf2881cd0adf830fc2740549b91c0a
Nickson K Rop <nickyrutto@gmail.com>
2024-04-01 07:49:37 -0700

    fix syntax error

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources(r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close_db(obj):
