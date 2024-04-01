0635220e9ccecb9ce220a20c616b931fa5671235
Nickson K Rop <nickyrutto@gmail.com>
2024-04-01 07:41:12 -0700

    create a CORS insatance allowing /* for 0.0.0.0

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,13 +3,14 @@
 flask web application
 """
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources(r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close_db(obj):
